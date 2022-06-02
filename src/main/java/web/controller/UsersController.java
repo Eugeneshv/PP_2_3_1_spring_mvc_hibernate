892bba1106da91fd39c2998a4a7487a5bc58adc3
Eugeneshv <e.kolyshev@gmail.com>
2022-06-02 23:10:09 +0300

    temp

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -1,15 +1,27 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UsersController {
 
     @GetMapping("/")
-    public String printUsersList() {
-
+    public String welcomePage(@RequestParam(value = "first", required = false) String first,
+            @RequestParam(value = "second", required = false) String second,
+            @RequestParam(value = "sign", required = false) String sign,
+            Model model) {
+// temporary
+        model.addAttribute("message", first  + "sign" + second);
 
         return "index";
+    }
+
+    @GetMapping("/listusers")
+    public String listUsers() {
+
+        return "listusers";
     }
 }
